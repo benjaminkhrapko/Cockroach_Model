2d856d0dfee6eb5c628ccf434725472aa6c2d8d2
Benjamin Khrapko <benjaminkhrapko@macbookair.resource.campus.njit.edu>
2026-09-11 13:40:15 -0400

    first commit

---
+++ Week1ABM.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+print(random.randint(0, 10))
+ap=np.array([0.0,0.0])
---
+++ readme.md
@@ -0,0 +1,6 @@
+To run the program:
+
+
+1. source venv/bin/activate
+2. pip install -r requirements.txt
+3. python3 Week1ABM.py
