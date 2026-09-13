6d01f34c9cb1a9235fd56f7ceef0f77f4df0c430
Benjamin Khrapko <benjaminkhrapko@MacBookAir.fios-router.home>
2026-09-13 10:35:42 -0400

    second commit

--- Week1ABM.py
+++ Week1ABM.py
@@ -15,10 +15,20 @@ all_positions[0]=ap
 
 for t in range(1,max_time+1):
         direction=np.random.normal(size=2)
-while np.all(direction==0):
-        direction=np.random.normal(size=2)
-new_direction=direction=normal(direction)
-new_position=ap+new_direction
-all_positions[t]=new_position
 
-ap=new_position
+        while np.all(direction==0):
+            direction=np.random.normal(size=2)
+        new_direction=normal(direction)
+        new_position=ap+new_direction
+        all_positions[t]=new_position
+        ap=new_position
+
+x = all_positions[:, 0]
+y = all_positions[:, 1]
+
+plt.plot(x, y)
+plt.title('Random Walk')
+plt.xlabel('X Position')
+plt.ylabel('Y Position')
+plt.axis('equal')
+plt.show()
